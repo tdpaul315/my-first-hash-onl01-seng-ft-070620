44e4a6cb09de9c4892e1b78eabc4bcbca0df3872
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-16 02:22:18 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -16,7 +16,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+ oil paintings[] # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
 

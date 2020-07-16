228256bfc49e39b43b2d781fbb61c2811208243b
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-16 03:38:21 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -27,7 +27,7 @@ def adding
                   "oil paintings" => 3
                  }
 
- shipping_manifest["muskets"] = 2
+ shipping_manifest[" muskets"] = 2
   
  shipping_manifest["gun_powder"] = 4
   

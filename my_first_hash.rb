1ee61cc5b1785ee5d9f1c6953f37485d371fb5c1
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-16 02:26:44 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,6 +26,8 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
+  shipping_manifest["muskets"] => 2 
+  
   # add 2 muskets to the shipping_manifest hash below
 
 
